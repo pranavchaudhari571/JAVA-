aa19ccc7683dedb77d6cbcfe2fed8409a90d3bef
pranavchaudhari571 <HP@DESKTOP-LEJ840E>
2023-11-02 15:53:53 +0530

    commit.

---
+++ Task Manager/src/Custom_Exc/Task_customEcx.java
@@ -0,0 +1,14 @@
+package Custom_Exc;
+
+public class Task_customEcx extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Task_customEcx(String str) {
+		// TODO Auto-generated constructor stub
+		super(str);
+	}
+
+}
